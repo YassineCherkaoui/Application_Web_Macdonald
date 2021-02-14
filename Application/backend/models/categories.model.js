e43c403ad9d4adfb399f0563b8c3526673e08367
YassineCherkaoui <yassin.cherkaoui2000@gmail.com>
2021-02-14 23:39:57 +0100

    updated

---
+++ Application/backend/models/categories.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const Category = new Schema({
+    nom: {
+        type: String,
+        required: true,
+        trim: true,
+    }
+
+}, {
+    versionKey: false
+});
+const CategoryList = mongoose.model("Category", Category);
+module.exports = CategoryList;
